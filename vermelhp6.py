c31611c5b89687dd507cc77ba68746e07382e934
davigabrielr <davi.roza@amf.edu.br>
2026-06-09 20:28:45 -0300

    Simplify input prompts for Fazenda arrays
    Updated input prompts for Fazenda A and B to remove position numbers.

---
+++ vermelhp6.py
@@ -0,0 +1,20 @@
+def soma_fazendas(a, b):
+    c = []
+
+  
+    for i in range(len(a)):
+        c.append(a[i] + b[i])
+    return c
+
+fazendaA = []
+fazendaB = []
+
+for i in range(5):
+    fazendaA.append(int(input(f"Fazenda A posição: ")))
+for i in range(5):
+    fazendaB.append(int(input(f"Fazenda B posição: ")))
+resultado = soma_fazendas(fazendaA, fazendaB)
+
+
+
+print("Array soma:", resultado)
