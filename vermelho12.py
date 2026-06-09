a263eecaf697c9349796dee34681b27e548ef16a
davigabrielr <davi.roza@amf.edu.br>
2026-06-09 20:41:31 -0300

    Create vermelho12.py

---
+++ vermelho12.py
@@ -0,0 +1,25 @@
+
+
+def calcular_valor(kg, preco):
+    valor = []
+
+  
+    for i in range(len(kg)):
+        valor.append(kg[i] * preco[i])
+
+    return valor
+
+kg_colhidos = []
+preco_por_kg = []
+
+for i in range(5):
+    kg_colhidos.append(float(input(f"Kg da árvore: ")))
+
+
+for i in range(5):
+    preco_por_kg.append(float(input(f"Preço por kg da árvore: ")))
+
+resultado = calcular_valor(kg_colhidos, preco_por_kg)
+
+print("Valor obtido por árvore:")
+print(resultado)
