f0535ffbc5fbdb2550ee4971d3dd113c6bc25c67
davigabrielr <davi.roza@amf.edu.br>
2026-06-16 09:07:54 -0300

    Implement counting of even and odd numbers
    Add function to count even and odd numbers in a list.

--- preto9.py
+++ preto9.py
@@ -1,1 +1,23 @@
 
+import random
+
+def contar_pares_impares(vetor):
+    pares = 0
+    impares = 0
+
+    for valor in vetor:
+        if valor % 2 == 0:
+            pares += 1
+        else:
+            impares += 1
+
+    print("Pares:", pares)
+    print("Ímpares:", impares)
+
+ids = []
+
+for i in range(10):
+    ids.append(random.randint(1, 50))
+
+print(ids)
+contar_pares_impares(ids)
